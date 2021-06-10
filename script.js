bc1b0987d9a1c267b3f8349852a05b5219746b9a
Sebastian Martinez <smariver@gmail.com>
2021-06-09 22:22:15 -0300

    A lot of changes Modified grid creator to be dynamic. Added prompt to let user select the size of the grid. Added rainbow generator. Added button to clear the grid and generate a new one with the users input. More changes.

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 const container = document.getElementById("grid-container");
-console.log(container);
+const resetButton = document.getElementById("reset-button");
 
-function makeGrid(rowNum, colNum){
-    for(let i = 0; i<rowNum; i++){
-        let row = document.createElement("div");
-        row.className = "row";
-        for(let j = 0; j<colNum; j++){
-            var col = document.createElement("div");
-            col.className = "col";
-            row.appendChild(col);
-        }
-        container.appendChild(row);
+resetButton.addEventListener("click", changeSize);
+
+window.addEventListener("load", defaultGrid);
+
+function defaultGrid(){
+    makeGrid2(5,5);
+}
+
+function makeGrid2(rowNum, colNum){
+    container.style.setProperty('--grid-rows', rowNum);
+    container.style.setProperty('--grid-cols', colNum);
+    for(let i = 0; i < (rowNum*colNum); i++){
+        let cell = document.createElement("div");
+        cell.addEventListener("mouseover", changeColor);
+        container.appendChild(cell).className = "grid-item";
     }
 }
 
-makeGrid(16,16);
+function clearGrid(){
+    const gridArray = Array.from(container.childNodes);
+    gridArray.forEach((element) => {
+        container.removeChild(element);
+    });
+}
+
+function changeSize(){
+    let newSize = prompt("Enter size");
+
+    if(newSize !== null){
+        newSize = parseInt(newSize)
+        if(newSize < 1 || newSize > 100 || !Number.isInteger(newSize)){
+            alert("Number must be between 1 and 100");
+            changeSize();
+        }
+        else {
+            clearGrid();
+            makeGrid2(newSize, newSize);
+        }
+    }
+}
+
+function changeColor(e){
+    e.target.style.backgroundColor = '#' + Math.random().toString(16).substr(-6);
+}
